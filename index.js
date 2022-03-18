130b482cea97b6966e4807c98da11d346ae86810
amadeamargo <amadeamargo@gmail.com>
2022-03-18 13:28:10 -0500

    added questions to index.js file

--- index.js
+++ index.js
@@ -3,10 +3,38 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const generateMarkdown = require('.')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: 'input',
+        message: "What is the title of your repository?",
+        name: "repository title"
+    },
+    {
+        type: 'input',
+        message: "Please give a brief description of your application?",
+        name: "description"
+    },
+    {
+        type: 'input',
+        message: "Who are the contributors of this application?",
+        name: "contributors"
+    }
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data);
+    console.log(markdown)
+    
+    fs.writeToFile(fileName, markdown, (err) => 
+        err ? console.error("Unable to write README file") : console.log ("Succesfully wrote README file0")
+    )
+}
 
 // TODO: Create a function to initialize app
 function init() {}
